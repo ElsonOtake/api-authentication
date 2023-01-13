1eb387aa68b96f9bc57cc47e807d3bf68fac3f7e
Elson Otake <elsonotake@yahoo.com.br>
2023-01-13 15:13:55 -0300

    Update to body params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,10 +15,10 @@ class Api::V1::UsersController < ApplicationController
 
   # POST /api/v1/users
   def create
-    @user = User.new(user_params)
+    @user = User.new(json_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -26,7 +26,7 @@ class Api::V1::UsersController < ApplicationController
 
   # PATCH/PUT /api/v1/users/1
   def update
-    if @user.update(user_params)
+    if @user.update(json_params)
       render json: @user
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -44,8 +44,9 @@ class Api::V1::UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.fetch(:user, {})
+    # Only allow a list of trusted JSON parameters through.
+    def json_params
+      allowed_data = %(email description).freeze
+      json_payload.select { |allow| allowed_data.include?(allow) }
     end
 end
