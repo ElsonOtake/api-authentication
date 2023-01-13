30b07ab8ba45c0cb41028de37595b32fafa7e650
Elson Otake <elsonotake@yahoo.com.br>
2023-01-13 14:21:56 -0300

    Add localhost

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -18,16 +18,17 @@ RSpec.configure do |config|
     'v1/swagger.yaml' => {
       openapi: '3.0.1',
       info: {
-        title: 'API V1',
+        title: 'Documentation API V1',
+        description: 'Sample app created for Medium article',
         version: 'v1'
       },
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: 'http://localhost:3000/',
           variables: {
             defaultHost: {
-              default: 'www.example.com'
+              default: 'localhost:3000'
             }
           }
         }
