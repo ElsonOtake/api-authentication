32c750004d1b824b5cb2644f50ff34582512a320
Elson Otake <elsonotake@yahoo.com.br>
2023-02-01 14:10:58 -0300

    Add authorize_request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::API
   
     HashWithIndifferentAccess.new(JSON.parse(request.raw_post))
   end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    if header
+      header = header.split.last
+      begin
+        @decoded = JsonWebToken.decode(header)
+        @current_user = User.find_by_id!(@decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound || JWT::DecodeError => e
+        render json: { errors: e.message }, status: :unauthorized
+      end
+    else
+      render json: { errors: 'Unauthorized user' }, status: :unauthorized
+    end
+  end
 end
