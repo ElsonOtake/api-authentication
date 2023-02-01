51b753b0a42fe096f993277d51a840a27195327a
Elson Otake <elsonotake@yahoo.com.br>
2023-02-01 14:14:14 -0300

    Add route to login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  post 'api/v1/auth/login', to: 'authentication#login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
